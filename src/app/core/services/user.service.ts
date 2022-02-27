0f2048c46bbe36f5a345e5b83628823d69eb6b2e
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-02-27 09:36:57 +0100

    cache users and use npm

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,51 +2,62 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { IUser } from '../interfaces/user.interface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  usersApiUrl = 'https://api.randomuser.me?results=10&seed=packt'
-  commentsJsonUrl = 'assets/data/comments.json'
-  constructor(private http: HttpClient) { }
+  usersApiUrl = 'https://api.randomuser.me?results=10&seed=packt';
+  commentsJsonUrl = 'assets/data/comments.json';
+  users: IUser[] = [];
+  constructor(private http: HttpClient) {}
 
   getUser(userId: string): Observable<IUser> {
-    return this.http.get<{results: IUser[]}>(this.usersApiUrl).pipe(
-      map((resp) => resp.results.find(user => {
-        return user.login.uuid === userId;
-      }))
-    )
+    return this.getSimilarUsers(null).pipe(
+      map((users) =>
+        users.find((user) => {
+          return user.login.uuid === userId;
+        })
+      )
+    );
   }
 
   getSimilarUsers(userId: string): Observable<IUser[]> {
-    return this.http.get<{results: IUser[]}>(
-      this.usersApiUrl
-    ).pipe(
-      map((resp) => resp.results.filter(user => user.login.uuid !== userId))
-    )
+    const obs = this.users.length
+      ? of({
+          results: this.users,
+        })
+      : this.http.get<{ results: IUser[] }>(this.usersApiUrl);
+    return obs.pipe(
+      map((resp) => {
+        this.users = resp.results;
+        return this.users.filter((user) => user.login.uuid !== userId);
+      })
+    );
   }
 
   searchUsers(username: string): Observable<IUser[]> {
-    return this.getSimilarUsers(null).pipe(map(users => {
-      if (!username) {
-        return users;
-      }
-      return users.filter(user => {
-        const query = username.toLowerCase();
-        return (
-          user.email.toLowerCase().includes(query) ||
-          user.name.first.toLowerCase().includes(query) ||
-          user.name.last.toLowerCase().includes(query)
-        )
+    return this.getSimilarUsers(null).pipe(
+      map((users) => {
+        if (!username) {
+          return users;
+        }
+        return users.filter((user) => {
+          const query = username.toLowerCase();
+          return (
+            user.email.toLowerCase().includes(query) ||
+            user.name.first.toLowerCase().includes(query) ||
+            user.name.last.toLowerCase().includes(query)
+          );
+        });
       })
-    }) )
+    );
   }
 
   getDataComments(): Observable<string> {
-    return this.http.get<{__comments: string}>(this.commentsJsonUrl).pipe(
-      map((resp) => resp.__comments)
-    )
+    return this.http
+      .get<{ __comments: string }>(this.commentsJsonUrl)
+      .pipe(map((resp) => resp.__comments));
   }
 }
